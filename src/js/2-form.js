0d5356e25556f9e33b260492a29fcd4e88519a67
Andrii Butenko <andrejbutik@gmail.com>
2025-05-25 13:45:38 +0200

    Started work

---
+++ src/js/2-form.js
@@ -0,0 +1,15 @@
+const formData = { email: "", message: "" }
+const form = document.querySelector("feedback-form");
+form.addEventListener("input", event => {
+    
+})
+
+
+
+
+
+
+
+
+
+localStorage.setItem("formData", JSON.stringify(formData));
